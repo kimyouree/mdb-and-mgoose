3d616c18e4e6f131cdcd0b85101d1f92334e72e6
kimyouree <kimyouree@iccloud.com>
2023-06-13 08:12:06 -0400

    add: Install and set up mongoose

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
+const mongoose = require("mongoose");
 
-
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+console.log(process.env.MONGO_URI)
 let Person;
 
 const createAndSavePerson = (done) => {
