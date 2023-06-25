bc5c0c917b82a407400eb7ec0121ad7af7d56266
kimyouree <kimyouree@iccloud.com>
2023-06-25 18:32:07 -0400

    add: Create many people with model.create()

--- myApp.js
+++ myApp.js
@@ -42,8 +42,20 @@ const createAndSavePerson = (done) => {
   })
 };
 
+// 4/12) Create many People with `Model.create()`
+const arrayOfPeople = [
+  {name: "Frankie", age: 74, favoriteFoods: ["Del Taco"]},
+  {name: "Sol", age: 76, favoriteFoods: ["roast chicken"]},
+  {name: "Robert", age: 78, favoriteFoods: ["wine"]}
+];
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, function(err, people) {
+    if (err) {
+      return console.error(err);
+    }
+    console.log(`This is people: ${people}`)
+    done(null , people);
+  })
 };
 
 const findPeopleByName = (personName, done) => {
